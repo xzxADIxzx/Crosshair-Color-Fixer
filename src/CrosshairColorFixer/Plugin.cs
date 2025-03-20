45c389758882adb05cc409661052e6ae208a7c3c
xzxADIxzx <76255437+xzxADIxzx@users.noreply.github.com>
2025-03-20 23:22:24 +0300

    add a bootloader class

--- src/CrosshairColorFixer/Plugin.cs
+++ src/CrosshairColorFixer/Plugin.cs
@@ -4,13 +4,18 @@ using BepInEx;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/// <summary> Bootloader class needed to avoid destroying the mod by the game. </summary>
+[BepInPlugin("xzxADIxzx.CorsshairColorFixer", "CrosshairColorFixer", "1.0.1")]
+public class PluginLoader : BaseUnityPlugin
+{
+    private void Awake() => SceneManager.sceneLoaded += (_, _) => Plugin.Instance.Invoke("ApplyFix", .1f);
+}
+
 /// <summary> Plugin class containing the whole logic of the mod. There isn't a lot, but what did you expect? </summary>
-[BepInPlugin("xzxADIxzx.CorsshairColorFixer", "CrosshairColorFixer", "1.0.0")]
-public class Plugin : BaseUnityPlugin
+[ConfigureSingleton(SingletonFlags.PersistAutoInstance)]
+public class Plugin : MonoSingleton<Plugin>
 {
     ColorBlindSettings cb => ColorBlindSettings.Instance;
-
-    private void Awake() => SceneManager.sceneLoaded += (_, _) => Invoke("ApplyFix", .1f);
 
     private void ApplyFix()
     {
